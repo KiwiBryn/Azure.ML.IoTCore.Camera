eb421e7d698dbcf9b29ac4d2bf17d123a427dbcd
Bryn Lewis <bryn.lewis@devMobile.co.nz>
2019-03-17 18:28:35 +1300

    First working version of Azure Storage upload

--- PhotoDigitalInputTriggerAzureStorage/StartupTask.cs
+++ PhotoDigitalInputTriggerAzureStorage/StartupTask.cs
@@ -1,25 +1,144 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Net.Http;
-using Windows.ApplicationModel.Background;
+﻿/*
+    Copyright ® 2019 Feb devMobile Software, All Rights Reserved
+ 
+    MIT License
 
-// The Background Application template is documented at http://go.microsoft.com/fwlink/?LinkID=533884&clcid=0x409
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
 
-namespace PhotoDigitalInputTriggerAzureStorage
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE
+
+*/
+namespace devMobile.Windows10IotCore.IoT.PhotoDigitalInputTriggerAzureStorage
 {
-    public sealed class StartupTask : IBackgroundTask
-    {
-        public void Run(IBackgroundTaskInstance taskInstance)
-        {
-            // 
-            // TODO: Insert code to perform background work
-            //
-            // If you start any asynchronous methods here, prevent the task
-            // from closing prematurely by using BackgroundTaskDeferral as
-            // described in http://aka.ms/backgroundtaskdeferral
-            //
-        }
-    }
+	using System;
+	using System.Diagnostics;
+
+	using Microsoft.WindowsAzure.Storage;
+	using Microsoft.WindowsAzure.Storage.Blob;
+
+	using Windows.ApplicationModel.Background;
+	using Windows.Devices.Gpio;
+	using Windows.Foundation.Diagnostics;
+	using Windows.Media.Capture;
+	using Windows.Media.MediaProperties;
+	using Windows.Storage;
+	using Windows.Storage.Streams;
+
+	public sealed class StartupTask : IBackgroundTask
+	{
+		private readonly LoggingChannel logging = new LoggingChannel("devMobile Photo Digital Input Trigger Azure Storage demo", null, new Guid("4bd2826e-54a1-4ba9-bf63-92b73ea1ac4a"));
+		private BackgroundTaskDeferral backgroundTaskDeferral = null;
+		private GpioPin InterruptGpioPin = null;
+		private const int InterruptPinNumber = 5;
+		private MediaCapture mediaCapture;
+		private const string ImageFilenameLatest = "latest.jpg";
+		private const string ImageFilenameFormat = "image{1:yyMMddhhmmss}.jpg";
+		private const string ContainerNameFormat = "{0}{1:yyMMdd}";
+		private volatile bool CameraBusy = false;
+		private const string AzureStorageConnectionString = "DefaultEndpointsProtocol=https;AccountName=deviceimages;AccountKey=8l7R5crjc3gvlAduJiJHhbKpvmux8e1r1r/JhyOaoI20FhliIjiNTV5QmXE1LGZCEqEHVg1nMsqQeV2cjlVEtg==;EndpointSuffix=core.windows.net";
+
+		public void Run(IBackgroundTaskInstance taskInstance)
+		{
+			this.logging.LogEvent("Application starting");
+
+			try
+			{
+				mediaCapture = new MediaCapture();
+				mediaCapture.InitializeAsync().AsTask().Wait();
+
+				GpioController gpioController = GpioController.GetDefault();
+				InterruptGpioPin = gpioController.OpenPin(InterruptPinNumber);
+				InterruptGpioPin.SetDriveMode(GpioPinDriveMode.InputPullUp);
+				InterruptGpioPin.ValueChanged += InterruptGpioPin_ValueChanged;
+			}
+			catch (Exception ex)
+			{
+				this.logging.LogMessage("Camera or digital input configuration failed " + ex.Message, LoggingLevel.Error);
+				return;
+			}
+
+			LoggingFields startupInformation = new LoggingFields();
+			startupInformation.AddString("PrimaryUse", mediaCapture.VideoDeviceController.PrimaryUse.ToString());
+			startupInformation.AddInt32("Interrupt pin", InterruptPinNumber);
+			this.logging.LogEvent("Application started", startupInformation);
+
+			//enable task to continue running in background
+			backgroundTaskDeferral = taskInstance.GetDeferral();
+		}
+
+		private async void InterruptGpioPin_ValueChanged(GpioPin sender, GpioPinValueChangedEventArgs args)
+		{
+			DateTime currentTime = DateTime.UtcNow;
+			Debug.WriteLine($"{DateTime.UtcNow.ToLongTimeString()} Digital Input Interrupt {sender.PinNumber} triggered {args.Edge}");
+
+			if (args.Edge == GpioPinEdge.RisingEdge)
+			{
+				return;
+			}
+
+			// Just incase - stop code being called while photo already in progress
+			if (CameraBusy)
+			{
+				return;
+			}
+			CameraBusy = true;
+
+			try
+			{
+				using (InMemoryRandomAccessStream captureStream = new InMemoryRandomAccessStream())
+				{
+					string filename = string.Format(ImageFilenameFormat, Environment.MachineName.ToLower(), currentTime);
+
+					StorageFile photoFile = await KnownFolders.PicturesLibrary.CreateFileAsync(filename, CreationCollisionOption.ReplaceExisting);
+					ImageEncodingProperties imageProperties = ImageEncodingProperties.CreateJpeg();
+					await mediaCapture.CapturePhotoToStorageFileAsync(imageProperties, photoFile);
+
+					LoggingFields imageInformation = new LoggingFields();
+					imageInformation.AddDateTime("TakenAtUTC", currentTime);
+					imageInformation.AddInt32("Pin", sender.PinNumber);
+					imageInformation.AddString("Path", photoFile.Path);
+					imageInformation.AddString("Filename", filename);
+					imageInformation.AddUInt32("Height", imageProperties.Height);
+					imageInformation.AddUInt32("Width", imageProperties.Width);
+					imageInformation.AddUInt64("Size", captureStream.Size);
+					this.logging.LogEvent("Captured image saved to storage", imageInformation);
+
+					CloudStorageAccount storageAccount = CloudStorageAccount.Parse(AzureStorageConnectionString);
+					CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+
+					string containername = string.Format(ContainerNameFormat, Environment.MachineName.ToLower(), currentTime);
+					CloudBlobContainer container = blobClient.GetContainerReference(containername);
+					await container.CreateIfNotExistsAsync();
+
+					CloudBlockBlob blockBlob = container.GetBlockBlobReference(filename);
+					await blockBlob.UploadFromFileAsync(photoFile);
+
+					blockBlob = container.GetBlockBlobReference(ImageFilenameLatest);
+					await blockBlob.UploadFromFileAsync(photoFile);
+				}
+			}
+			catch (Exception ex)
+			{
+				this.logging.LogMessage("Camera photo or save failed " + ex.Message, LoggingLevel.Error);
+			}
+			finally
+			{
+				CameraBusy = false;
+			}
+		}
+	}
 }
