93e1c84d93adde563d1f44f96bdf00e5c1b7f0bc
Bryn Lewis <bryn.lewis@devMobile.co.nz>
2019-03-23 17:34:38 +1300

    Updated the version njumber and some of the descriptive fields

--- PhotoDigitalInputTriggerAzureStorage/Properties/AssemblyInfo.cs
+++ PhotoDigitalInputTriggerAzureStorage/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("PhotoDigitalInputTriggerAzureStorage")]
+[assembly: AssemblyTitle("USB Camera Digital Input Trigger Upload to Azure Storage")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("PhotoDigitalInputTriggerAzureStorage")]
+[assembly: AssemblyCompany("devMobile Software")]
+[assembly: AssemblyProduct("USBCameraDigitalInputTriggerAzureStorage")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
