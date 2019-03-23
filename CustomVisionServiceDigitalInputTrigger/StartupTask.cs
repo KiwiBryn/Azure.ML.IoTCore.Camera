beac70df49ebe9e9c2272afc59644e62296fe96c
Bryn Lewis <bryn.lewis@devMobile.co.nz>
2019-03-24 12:56:42 +1300

    Start of custom vision service cleint

--- CustomVisionServiceDigitalInputTrigger/StartupTask.cs
+++ CustomVisionServiceDigitalInputTrigger/StartupTask.cs
@@ -26,45 +26,133 @@ namespace devMobile.Windows10IotCore.IoT.CustomVisionServiceDigitalInputTrigger
 {
 	using System;
 	using System.Diagnostics;
+	using System.IO;
+	using System.Linq;
+	using System.Net.NetworkInformation;
+
+	using Microsoft.Extensions.Configuration;
+
+	using Windows.ApplicationModel;
 	using Windows.ApplicationModel.Background;
 	using Windows.Devices.Gpio;
 	using Windows.Foundation.Diagnostics;
 	using Windows.Media.Capture;
 	using Windows.Media.MediaProperties;
 	using Windows.Storage;
+	using Windows.System;
 
 	public sealed class StartupTask : IBackgroundTask
 	{
-		private readonly LoggingChannel logging = new LoggingChannel("devMobile Photo Digital Input Trigger demo", null, new Guid("4bd2826e-54a1-4ba9-bf63-92b73ea1ac4a"));
 		private BackgroundTaskDeferral backgroundTaskDeferral = null;
-		private GpioPin InterruptGpioPin = null;
-		private const int InterruptPinNumber = 5;
+		private readonly LoggingChannel logging = new LoggingChannel("devMobile Photo Digital Input Trigger Custom Vision", null, new Guid("4bd2826e-54a1-4ba9-bf63-92b73ea1ac4a"));
+		private const string ConfigurationFilename = "appsettings.json";
+		private GpioPin interruptGpioPin = null;
+		private GpioPinEdge interruptTriggerOn = GpioPinEdge.RisingEdge;
+		private int interruptPinNumber;
+		private TimeSpan debounceTimeout;
+		private DateTime imageLastCapturedAtUtc = DateTime.MinValue;
 		private MediaCapture mediaCapture;
+		private string deviceMacAddress;
+		//private string azureStorageConnectionString;
 		private const string ImageFilenameFormat = "Image{0:yyMMddhhmmss}.jpg";
 		private volatile bool CameraBusy = false;
 
 		public void Run(IBackgroundTaskInstance taskInstance)
 		{
-			Debug.WriteLine("Application startup");
+			StorageFolder localFolder = ApplicationData.Current.LocalFolder;
+
+			this.logging.LogEvent("Application starting");
+
+			// Log the Application build, OS version information etc.
+			LoggingFields startupInformation = new LoggingFields();
+			startupInformation.AddString("Timezone", TimeZoneSettings.CurrentTimeZoneDisplayName);
+			startupInformation.AddString("OSVersion", Environment.OSVersion.VersionString);
+			startupInformation.AddString("MachineName", Environment.MachineName);
+
+			// This is from the application manifest 
+			Package package = Package.Current;
+			PackageId packageId = package.Id;
+			PackageVersion version = packageId.Version;
+			startupInformation.AddString("ApplicationVersion", string.Format($"{version.Major}.{version.Minor}.{version.Build}.{version.Revision}"));
+
+			// ethernet mac address
+			deviceMacAddress = NetworkInterface.GetAllNetworkInterfaces()
+				 .Where(i => i.NetworkInterfaceType.ToString().ToLower().Contains("ethernet"))
+				 .FirstOrDefault()
+				 ?.GetPhysicalAddress().ToString();
+
+			// remove unsupported charachers from MacAddress
+			deviceMacAddress = deviceMacAddress.Replace("-", "").Replace(" ", "").Replace(":", "");
+			startupInformation.AddString("MacAddress", deviceMacAddress);
+
+			try
+			{
+				// see if the configuration file is present if not copy minimal sample one from application directory
+				if (localFolder.TryGetItemAsync(ConfigurationFilename).AsTask().Result == null)
+				{
+					StorageFile templateConfigurationfile = Package.Current.InstalledLocation.GetFileAsync(ConfigurationFilename).AsTask().Result;
+					templateConfigurationfile.CopyAsync(localFolder, ConfigurationFilename).AsTask();
+				}
+
+				IConfiguration configuration = new ConfigurationBuilder().AddJsonFile(Path.Combine(localFolder.Path, ConfigurationFilename), false, true).Build();
+
+				/*
+				azureStorageConnectionString = configuration.GetSection("AzureStorageConnectionString").Value;
+				startupInformation.AddString("AzureStorageConnectionString", azureStorageConnectionString);
+
+				azureStorageContainerNameLatestFormat = configuration.GetSection("AzureContainerNameFormatLatest").Value;
+				startupInformation.AddString("ContainerNameLatestFormat", azureStorageContainerNameLatestFormat);
+
+				azureStorageimageFilenameLatestFormat = configuration.GetSection("AzureImageFilenameFormatLatest").Value;
+				startupInformation.AddString("ImageFilenameLatestFormat", azureStorageimageFilenameLatestFormat);
+
+				azureStorageContainerNameHistoryFormat = configuration.GetSection("AzureContainerNameFormatHistory").Value;
+				startupInformation.AddString("ContainerNameHistoryFormat", azureStorageContainerNameHistoryFormat);
+
+				azureStorageImageFilenameHistoryFormat = configuration.GetSection("AzureImageFilenameFormatHistory").Value;
+				startupInformation.AddString("ImageFilenameHistoryFormat", azureStorageImageFilenameHistoryFormat);
+				*/
+
+				interruptPinNumber = int.Parse(configuration.GetSection("InterruptPinNumber").Value);
+				startupInformation.AddInt32("Interrupt pin", interruptPinNumber);
+
+				interruptTriggerOn = (GpioPinEdge)Enum.Parse(typeof(GpioPinEdge), configuration.GetSection("interruptTriggerOn").Value);
+				startupInformation.AddString("Interrupt Trigger on", interruptTriggerOn.ToString());
+
+				debounceTimeout = TimeSpan.Parse(configuration.GetSection("debounceTimeout").Value);
+				startupInformation.AddTimeSpan("Debounce timeout", debounceTimeout);
+			}
+			catch (Exception ex)
+			{
+				this.logging.LogMessage("JSON configuration file load or settings retrieval failed " + ex.Message, LoggingLevel.Error);
+				return;
+			}
 
 			try
 			{
 				mediaCapture = new MediaCapture();
 				mediaCapture.InitializeAsync().AsTask().Wait();
-				Debug.WriteLine("Camera configuration success");
-
-				GpioController gpioController = GpioController.GetDefault();
-
-				InterruptGpioPin = gpioController.OpenPin(InterruptPinNumber);
-				InterruptGpioPin.SetDriveMode(GpioPinDriveMode.InputPullUp);
-				InterruptGpioPin.ValueChanged += InterruptGpioPin_ValueChanged;
-				Debug.WriteLine("Digital Input Interrupt configuration success");
 			}
 			catch (Exception ex)
 			{
-				this.logging.LogMessage("Camera or digital input configuration failed " + ex.Message, LoggingLevel.Error);
+				this.logging.LogMessage("Camera configuration failed " + ex.Message, LoggingLevel.Error);
 				return;
 			}
+
+			try
+			{
+				GpioController gpioController = GpioController.GetDefault();
+				interruptGpioPin = gpioController.OpenPin(interruptPinNumber);
+				interruptGpioPin.SetDriveMode(GpioPinDriveMode.InputPullUp);
+				interruptGpioPin.ValueChanged += InterruptGpioPin_ValueChanged;
+			}
+			catch (Exception ex)
+			{
+				this.logging.LogMessage("Digital input configuration failed " + ex.Message, LoggingLevel.Error);
+				return;
+			}
+
+			this.logging.LogEvent("Application started", startupInformation);
 
 			//enable task to continue running in background
 			backgroundTaskDeferral = taskInstance.GetDeferral();
@@ -75,10 +163,17 @@ namespace devMobile.Windows10IotCore.IoT.CustomVisionServiceDigitalInputTrigger
 			DateTime currentTime = DateTime.UtcNow;
 			Debug.WriteLine($"Digital Input Interrupt {sender.PinNumber} triggered {args.Edge}");
 
-			if (args.Edge == GpioPinEdge.RisingEdge)
+			if (args.Edge == interruptTriggerOn)
 			{
 				return;
 			}
+
+			// Check that enough time has passed for picture to be taken
+			if ((currentTime - imageLastCapturedAtUtc) < debounceTimeout)
+			{
+				return;
+			}
+			imageLastCapturedAtUtc = currentTime;
 
 			// Just incase - stop code being called while photo already in progress
 			if (CameraBusy)
@@ -117,8 +212,10 @@ namespace devMobile.Windows10IotCore.IoT.CustomVisionServiceDigitalInputTrigger
 			{
 				this.logging.LogMessage("Camera photo or save failed " + ex.Message, LoggingLevel.Error);
 			}
-
-			CameraBusy = false;
+			finally
+			{
+				CameraBusy = false;
+			}
 		}
 	}
 }
