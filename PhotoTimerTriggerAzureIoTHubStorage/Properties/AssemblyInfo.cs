69cb104a87f0e7a9c3a586d6a76652744adcd142
Bryn Lewis <bryn.lewis@devMobile.co.nz>
2019-04-01 21:34:27 +1300

    Updated version number, assembly title and company information

--- PhotoTimerTriggerAzureIoTHubStorage/Properties/AssemblyInfo.cs
+++ PhotoTimerTriggerAzureIoTHubStorage/Properties/AssemblyInfo.cs
@@ -5,10 +5,10 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("PhotoTimerTriggerAzureIoTHubStorage")]
+[assembly: AssemblyTitle("USB Camera Timer Trigger Upload to Azure IoT Hub Storage")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("devMobile Software")]
 [assembly: AssemblyProduct("PhotoTimerTriggerAzureIoTHubStorage")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
