143d6a8dd74d4651949c08a80d1975a1ee3307c8
Bryn Lewis <bryn.lewis@devMobile.co.nz>
2019-04-02 15:58:27 +1300

    Frist working version with twin functionality for uploading device properties and downloading settings. Also implemented commands for remote forcing the device to reboot and capturing an image

--- PhotoTimerTriggerAzureStorage/Properties/AssemblyInfo.cs
+++ PhotoTimerTriggerAzureStorage/Properties/AssemblyInfo.cs
@@ -5,10 +5,10 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("PhotoTimerTriggerAzureStorage")]
+[assembly: AssemblyTitle("USB Camera Timer Trigger Upload to Azure Storage")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("devMobile Software")]
 [assembly: AssemblyProduct("PhotoTimerTriggerAzureStorage")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
