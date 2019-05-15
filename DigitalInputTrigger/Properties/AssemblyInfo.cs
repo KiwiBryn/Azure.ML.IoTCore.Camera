30a1b0f42850aa4d6cba3a71bc090cd82bb418bb
Bryn Lewis <bryn.lewis@devMobile.co.nz>
2019-05-15 20:10:14 +1200

    Added StyleCop nuget and fixed the complaints. The (c) notice thing bugs me as I got IDE to insert it.

--- DigitalInputTrigger/Properties/AssemblyInfo.cs
+++ DigitalInputTrigger/Properties/AssemblyInfo.cs
@@ -1,15 +1,37 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+﻿// <copyright file="AssemblyInfo.cs" company="devMobile Software">
+// Copyright ® 2019 Feb devMobile Software, All Rights Reserved
+//
+//  MIT License
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE"
+//
+// </copyright>
+using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("DigitalInputTrigger")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Digital Input Interrupt Test Harness")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DigitalInputTrigger")]
+[assembly: AssemblyCompany("devMobile Software")]
+[assembly: AssemblyProduct("devMobile Software Digitial Input Interrupt Test Harness")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -17,13 +39,13 @@ using System.Runtime.InteropServices;
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version 
+//      Minor Version
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: ComVisible(false)]
--- DigitalInputTrigger/StartupTask.cs
+++ DigitalInputTrigger/StartupTask.cs
@@ -1,27 +1,26 @@
-﻿/*
-    Copyright ® 2019 Feb devMobile Software, All Rights Reserved
- 
-    MIT License
-
-    Permission is hereby granted, free of charge, to any person obtaining a copy
-    of this software and associated documentation files (the "Software"), to deal
-    in the Software without restriction, including without limitation the rights
-    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    copies of the Software, and to permit persons to whom the Software is
-    furnished to do so, subject to the following conditions:
-
-    The above copyright notice and this permission notice shall be included in all
-    copies or substantial portions of the Software.
-
-    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-    SOFTWARE
-
-*/
+﻿// <copyright file="StartupTask.cs" company="devMobile Software">
+// Copyright ® 2019 Feb devMobile Software, All Rights Reserved
+//
+//  MIT License
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE"
+//
+// </copyright>
 namespace devMobile.Windows10IotCore.IoT.DigitalInputTrigger
 {
 	using System;
@@ -31,9 +30,9 @@ namespace devMobile.Windows10IotCore.IoT.DigitalInputTrigger
 
 	public sealed class StartupTask : IBackgroundTask
 	{
+		private const int InterruptPinNumber = 115; // G2 on DB410C
+		private GpioPin interruptGpioPin = null;
 		private BackgroundTaskDeferral backgroundTaskDeferral = null;
-		private GpioPin InterruptGpioPin = null;
-		private const int InterruptPinNumber = 5;
 
 		public void Run(IBackgroundTaskInstance taskInstance)
 		{
@@ -43,9 +42,9 @@ namespace devMobile.Windows10IotCore.IoT.DigitalInputTrigger
 			{
 				GpioController gpioController = GpioController.GetDefault();
 
-				InterruptGpioPin = gpioController.OpenPin(InterruptPinNumber);
-				InterruptGpioPin.SetDriveMode(GpioPinDriveMode.InputPullUp);
-				InterruptGpioPin.ValueChanged += InterruptGpioPin_ValueChanged;
+				this.interruptGpioPin = gpioController.OpenPin(InterruptPinNumber);
+				this.interruptGpioPin.SetDriveMode(GpioPinDriveMode.InputPullUp);
+				this.interruptGpioPin.ValueChanged += this.InterruptGpioPin_ValueChanged;
 
 				Debug.WriteLine("Digital Input Interrupt configuration success");
 			}
@@ -55,8 +54,8 @@ namespace devMobile.Windows10IotCore.IoT.DigitalInputTrigger
 				return;
 			}
 
-			//enable task to continue running in background
-			backgroundTaskDeferral = taskInstance.GetDeferral();
+			// enable task to continue running in background
+			this.backgroundTaskDeferral = taskInstance.GetDeferral();
 		}
 
 		private void InterruptGpioPin_ValueChanged(GpioPin sender, GpioPinValueChangedEventArgs args)
