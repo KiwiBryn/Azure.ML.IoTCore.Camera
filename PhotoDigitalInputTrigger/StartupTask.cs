c9dea146f398b088aad9dd2ee3bd3604be0b13b6
Bryn Lewis <bryn.lewis@devMobile.co.nz>
2019-05-15 20:12:48 +1200

    Added StyleCop Nuget fixed the complaints

--- PhotoDigitalInputTrigger/StartupTask.cs
+++ PhotoDigitalInputTrigger/StartupTask.cs
@@ -1,27 +1,27 @@
-﻿/*
-    Copyright ® 2019 Feb devMobile Software, All Rights Reserved
- 
-    MIT License
+﻿// <copyright file="StartupTask.cs" company="devMobile Software">
+// Copyright ® 2019 Feb devMobile Software, All Rights Reserved
+//
+//  MIT License
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE"
+//
+// </copyright>
 
-    Permission is hereby granted, free of charge, to any person obtaining a copy
-    of this software and associated documentation files (the "Software"), to deal
-    in the Software without restriction, including without limitation the rights
-    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    copies of the Software, and to permit persons to whom the Software is
-    furnished to do so, subject to the following conditions:
-
-    The above copyright notice and this permission notice shall be included in all
-    copies or substantial portions of the Software.
-
-    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-    SOFTWARE
-
-*/
 namespace devMobile.Windows10IotCore.IoT.PhotoDigitalInputTrigger
 {
 	using System;
@@ -35,13 +35,13 @@ namespace devMobile.Windows10IotCore.IoT.PhotoDigitalInputTrigger
 
 	public sealed class StartupTask : IBackgroundTask
 	{
-		private readonly LoggingChannel logging = new LoggingChannel("devMobile Photo Digital Input demo", null, new Guid("4bd2826e-54a1-4ba9-bf63-92b73ea1ac4a"));
-		private BackgroundTaskDeferral backgroundTaskDeferral = null;
-		private GpioPin InterruptGpioPin = null;
-		private const int InterruptPinNumber = 5;
-		private MediaCapture mediaCapture;
 		private const string ImageFilenameFormat = "Image{0:yyMMddhhmmss}.jpg";
-		private volatile bool CameraBusy = false;
+		private const int InterruptPinNumber = 115; // G2 on DB410C;
+		private readonly LoggingChannel logging = new LoggingChannel("devMobile Photo Digital Input demo", null, new Guid("4bd2826e-54a1-4ba9-bf63-92b73ea1ac4a"));
+		private GpioPin interruptGpioPin = null;
+		private MediaCapture mediaCapture;
+		private volatile bool cameraBusy = false;
+		private BackgroundTaskDeferral backgroundTaskDeferral = null;
 
 		public void Run(IBackgroundTaskInstance taskInstance)
 		{
@@ -51,15 +51,15 @@ namespace devMobile.Windows10IotCore.IoT.PhotoDigitalInputTrigger
 
 			try
 			{
-				mediaCapture = new MediaCapture();
-				mediaCapture.InitializeAsync().AsTask().Wait();
+				this.mediaCapture = new MediaCapture();
+				this.mediaCapture.InitializeAsync().AsTask().Wait();
 				Debug.WriteLine("Camera configuration success");
 
 				GpioController gpioController = GpioController.GetDefault();
 
-				InterruptGpioPin = gpioController.OpenPin(InterruptPinNumber);
-				InterruptGpioPin.SetDriveMode(GpioPinDriveMode.InputPullUp);
-				InterruptGpioPin.ValueChanged += InterruptGpioPin_ValueChanged;
+				this.interruptGpioPin = gpioController.OpenPin(InterruptPinNumber);
+				this.interruptGpioPin.SetDriveMode(GpioPinDriveMode.InputPullUp);
+				this.interruptGpioPin.ValueChanged += this.InterruptGpioPin_ValueChanged;
 				Debug.WriteLine("Digital Input Interrupt configuration success");
 			}
 			catch (Exception ex)
@@ -68,13 +68,13 @@ namespace devMobile.Windows10IotCore.IoT.PhotoDigitalInputTrigger
 				return;
 			}
 
-			startupInformation.AddString("PrimaryUse", mediaCapture.VideoDeviceController.PrimaryUse.ToString());
+			startupInformation.AddString("PrimaryUse", this.mediaCapture.VideoDeviceController.PrimaryUse.ToString());
 			startupInformation.AddInt32("Interrupt pin", InterruptPinNumber);
 
 			this.logging.LogEvent("Application started", startupInformation);
 
-			//enable task to continue running in background
-			backgroundTaskDeferral = taskInstance.GetDeferral();
+			// enable task to continue running in background
+			this.backgroundTaskDeferral = taskInstance.GetDeferral();
 		}
 
 		private async void InterruptGpioPin_ValueChanged(GpioPin sender, GpioPinValueChangedEventArgs args)
@@ -88,11 +88,12 @@ namespace devMobile.Windows10IotCore.IoT.PhotoDigitalInputTrigger
 			}
 
 			// Just incase - stop code being called while photo already in progress
-			if (CameraBusy)
+			if (this.cameraBusy)
 			{
 				return;
 			}
-			CameraBusy = true;
+
+			this.cameraBusy = true;
 
 			try
 			{
@@ -100,7 +101,7 @@ namespace devMobile.Windows10IotCore.IoT.PhotoDigitalInputTrigger
 
 				IStorageFile photoFile = await KnownFolders.PicturesLibrary.CreateFileAsync(filename, CreationCollisionOption.ReplaceExisting);
 				ImageEncodingProperties imageProperties = ImageEncodingProperties.CreateJpeg();
-				await mediaCapture.CapturePhotoToStorageFileAsync(imageProperties, photoFile);
+				await this.mediaCapture.CapturePhotoToStorageFileAsync(imageProperties, photoFile);
 
 				LoggingFields imageInformation = new LoggingFields();
 
@@ -112,10 +113,10 @@ namespace devMobile.Windows10IotCore.IoT.PhotoDigitalInputTrigger
 			}
 			catch (Exception ex)
 			{
-				this.logging.LogMessage("Camera photo or save failed " + ex.Message, LoggingLevel.Error);
+				this.logging.LogMessage("Image capture or save failed " + ex.Message, LoggingLevel.Error);
 			}
 
-			CameraBusy = false;
+			this.cameraBusy = false;
 		}
 	}
 }
